2fe6ab6387f27dfb52aa70eeb3185f38711ff7fb
David-Aldair <d.aldair.quinonez@gmail.com>
2025-10-23 11:37:52 -0400

    Finished the 1st static method, and now finishing the 2nd static method

--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -1,6 +1,39 @@
 package com.pluralsight;
 
 public class NameFormatter {
+
+    //private constructor so you can't instantiate the class
+    private NameFormatter(){}
+
+    //static method for firstName and lastName
+    public static String format(String firstName,
+                                String lastName){
+
+        //if the first or last name is missing, returning an empty string
+        if(firstName.isEmpty() || lastName.isEmpty()){
+            return "";
+        }
+        //formatting correctly
+        return lastName + ", " + firstName;
+    }
+    //static method for prefix, firstName, middleName, lastName, suffx
+    public static String format(String prefix,
+                                String firstName,
+                                String middleName,
+                                String lastName,
+                                String suffix){
+         if(firstName.isEmpty() || lastName.isEmpty()){
+            return "";
+         }
+         //easier for more efficient concatenation
+         StringBuilder stringBuild = new StringBuilder();
+
+
+
+
+
+
+    }
 }
 
 
